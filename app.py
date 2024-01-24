81e8c29df2cd1f020a25e105a9eb895338ad061d
mannan-rana <mannanrana850@gmail.com>
2024-01-24 18:43:13 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify, request, send_from_directory
+from flask_cors import CORS
+
+app = Flask(__name__, static_folder='client')
+CORS(app)  # This line enables CORS for all routes
+
+# In-memory storage for expenses
+expenses = []
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/add-expense', methods=['POST'])
+def add_expense():
+    expense = request.json
+    expenses.append(expense)
+    return jsonify(expense), 201
+
+@app.route('/expenses', methods=['GET'])
+def get_expenses():
+    return jsonify(expenses)
+
+if __name__ == '__main__':
+    app.run(debug=True)
